ee23edccbe5c3966fc25f440be4587068c31e710
Louis Gill <lgill7@cnm.edu>
2015-10-25 21:13:53 -0600

    changed headings

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,16 +7,17 @@
 	</head>
 	<body>
 		<div class="boxed">
-		<h1>Purpose</h1>
-		<p>I would like my website to become a home for all things fantasy basketball. People will use its sagely wisdom to gain the upper hand against their opponents and I will become revered as a fantasy basketball guru.</p>
-		<hr>
-		<h1>Audience</h1>
-		<p>The primary user of the site is someone who is a hardcore player of fantasy basketball. The site will target those players who wish to go beyond casual enjoyment and use deep analysis and analytics to compete at a higher level.</p>
-		<p>As far as potential employers go, those in the online sports media industry may be the most enamored by the site. However, other potential employers and clients should ideally be impressed by the sleek style and concise nature of the site.</p>
-		<hr>
-		<h1>Goal</h1>
-		<p>Most importantly, this website should become something that I can showcase to promote my work, whether it be in an effort to attract freelance clients, acquire gainful employment, or garner an internship.</p>
-		<p>The auxiliary goal of the website is to grow it into a full-fledged business. This may happen slowly as it will take extra time to generate the content necessary to do so.</p>
+			<h1>fullcourtfantasy.com</h1>
+			<h2>Purpose</h2>
+			<p>I would like my website to become a home for all things fantasy basketball. People will use its sagely wisdom to gain the upper hand against their opponents and I will become revered as a fantasy basketball guru.</p>
+			<hr>
+			<h2>Audience</h2>
+			<p>The primary user of the site is someone who is a hardcore player of fantasy basketball. The site will target those players who wish to go beyond casual enjoyment and use deep analysis and analytics to compete at a higher level.</p>
+			<p>As far as potential employers go, those in the online sports media industry may be the most enamored by the site. However, other potential employers and clients should ideally be impressed by the sleek style and concise nature of the site.</p>
+			<hr>
+			<h2>Goal</h2>
+			<p>Most importantly, this website should become something that I can showcase to promote my work, whether it be in an effort to attract freelance clients, acquire gainful employment, or garner an internship.</p>
+			<p>The auxiliary goal of the website is to grow it into a full-fledged business. This may happen slowly as it will take extra time to generate the content necessary to do so.</p>
 		</div>
 	</body>
 </html>

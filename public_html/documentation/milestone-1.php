916b275abf20251c2e6e905568d46c4f13d5eb04
Louis Gill <lgill7@cnm.edu>
2015-10-25 20:03:16 -0600

    added Purpose section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,10 @@
 		<title>PWP Milestone 1</title>
 	</head>
 	<body>
-		<h1></h1>
+		<h1>Purpose</h1>
+		<p>I would like my website to become a home for all things fantasy basketball. People will use its sagely wisdom to gain the upper hand against their opponents and I will become revered as a fantasy basketball guru..</p>
+		<h1>Audience</h1>
+		<p></p>
+		<h1>Goal</h1>
 	</body>
 </html>

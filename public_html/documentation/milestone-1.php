c71459068015808316b11adce39c9ae95d9a80d0
Louis Gill <lgill7@cnm.edu>
2015-10-25 20:29:37 -0600

    added Audience & Goal sections

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,7 +8,10 @@
 		<h1>Purpose</h1>
 		<p>I would like my website to become a home for all things fantasy basketball. People will use its sagely wisdom to gain the upper hand against their opponents and I will become revered as a fantasy basketball guru..</p>
 		<h1>Audience</h1>
-		<p></p>
+		<p>The primary user of the site is someone who is a hardcore player of fantasy basketball. The site will target those players who wish to go beyond casual enjoyment and use deep analysis and analytics to compete at a higher level.</p>
+		<p>As far as potential employers go, those in the online sports media industry may be the most enamored by the site. However, other potential employers and clients should ideally be impressed by the sleek style and concise nature of the site.</p>
 		<h1>Goal</h1>
+		<p>Most importantly, this website should become something that I can showcase to promote my work, whether it be in an effort to attract freelance clients, acquire gainful employment, or garner an internship.</p>
+		<p>The auxiliary goal of the website is to grow it into a full-fledged business. This may happen slowly as it will take extra time to generate the content necessary to do so.</p>
 	</body>
 </html>

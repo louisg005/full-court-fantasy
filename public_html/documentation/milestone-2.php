7839aabec6d48c26bd21b34157c73f171976a735
Louis Gill <lgill7@cnm.edu>
2015-11-01 17:15:48 -0700

    began Use Case

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 2</h1>
+		<hr>
+		<p>Created by: Louis Gill</p>
+		<p>Created for: DDC Bootcamp, P.W. Project</p>
+		<p>Date: 11/1/2015</p>
+		<p>URL: http://fullcourtfantasy.com</p>
+		<h2>Introduction</h2>
+		<hr>
+		<p>The following document outlines the User Experience and Interaction Design for "fullcourtfantasy.com". This project is a source of tips and advice for fantasy basketball enthusiasts. This site is designed with an eccentric audience in mind and is targeted to individuals who take fantasy basketball very seriously. The primary goal of this site is for users of the site to remain connected and dependent on the site's information, be it through the email subscription list or the connected social media profiles. The secondary goal of this site is to showcase my work and to contribute to a polished professional online presence.</p>
+		<h2>Persona</h2>
+		<hr>
+		<p><strong>Name: </strong>Gary Barnidge</p>
+		<p><strong>Age: </strong>31</p>
+		<p><strong>Profession: </strong>Junior partner at BenJarvus Green-Ellis & Associates. Gary is saddled with much of the busywork and his free time at the office is almost nonexistent.</p>
+		<p><strong>Technology: </strong>Always on the go, Gary is attached to his Motorola Droid Turbo running Android 5.1 with an unlimited 4G data connection</p>
+		<p><strong>Attitudes and Behaviors: </strong>As someone with so much work and so little time, Gary values being able to maximize his time while staying in touch with friends via the competition of fantasy basketball. Gary finds a sliver of peace while on a train into the city every morning. Gary is an active social media user, using it as an active feed of basketball news and to talk trash against his opponents.</p>
+		<p><strong>Frustrations and Needs: </strong>Gary's services are so in-demand that he essentially cannot access the internet while he is at work. Gary needs to find a quality, serialized source of fantasy basketball news which can be consumed in five minutes or less.</p>
+		<p><strong>Goals: </strong>Gary is almost willing to do anything in order to garner his first fantasy basketball championship ring-- save losing his job. Can Gary, during his morning commute, absorb the relevant fantasy basketball information necessary to stay competitive in his league? Gary needs to know, via extremely brief interactions with the site, which players he should add or drop on any particular day.</p>
+		<h2>Use Case</h2>
+		<hr>
+		<p>Gary Barnidge is on the train on his way to work on a Friday. He has a fantasy basketball draft immediately after he gets off of work and has not prepared at all. Gary heard about the site from last year's champion and decides to give it a try. He leans away from the smelly man next to him, pulls out his Motorola Droid Turbo, and enters the Full-Court Fantasy URL. Here is an outline of Gary's interaction with the site:</p>
+		<ul>
+			<li>1. </li>
+			<li>2. </li>
+			<li>3. </li>
+		</ul>
+	</body>
+</html>

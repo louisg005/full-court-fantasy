e6f902e0d7183d886e79ebefe5966a14544dd9e5
Louis Gill <lgill7@cnm.edu>
2015-11-15 22:18:31 -0700

    started milestone 3

---
+++ public_html/documentation/milestone-3.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 3</title>
+	</head>
+	<body>
+		<p>wireframes (for mobile AND desktop) for each page of the site!!!!</p>
+	</body>
+</html>

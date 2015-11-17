7ace2a35e5986b8f048bcab0b8ef59f9e2e8003b
Louis Gill <lgill7@cnm.edu>
2015-11-16 21:30:51 -0700

    added headers

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -5,6 +5,24 @@
 		<title>Milestone 3</title>
 	</head>
 	<body>
-		<p>wireframes (for mobile AND desktop) for each page of the site!!!!</p>
+		<h1>Home Page</h1>
+		<hr>
+		<h3>Mobile</h3>
+		<h3>Desktop</h3>
+
+		<h1>News</h1>
+		<hr>
+		<h3>Mobile</h3>
+		<h3>Desktop</h3>
+
+		<h1>Rankings</h1>
+		<hr>
+		<h3>Mobile</h3>
+		<h3>Desktop</h3>
+
+		<h1>Articles</h1>
+		<hr>
+		<h3>Mobile</h3>
+		<h3>Desktop</h3>
 	</body>
 </html>

fe067cb631d342aeeeb63ea0a1e46681b4d69fb6
Louis Gill <lgill7@cnm.edu>
2015-12-13 23:14:26 -0700

    deleted side-panel

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$PAGE_TITLE = "My Capstone Project";
+require_once("php/template/utilities.php");
+?>
+<div class="container">
+	<header>
+		<?php require_once("php/template/header.php"); ?>
+	</header>
+
+	<div class="row">
+
+		<section class="main-content col-md-9">
+			<h1>Hello World</h1>
+			<p>This is the main page content here.</p>
+		</section>
+	</div>
+
+	<footer class="footer">
+		<?php require_once("php/template/footer.php"); ?>
+	</footer>
+</div><!-- .container -->
+
+</body>
+</html>
